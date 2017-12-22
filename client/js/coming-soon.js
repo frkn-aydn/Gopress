d6aa8cc84866b5b2e9768a5e58332a5020d5ccbe
Muhammed Furkan Aydın <furkanaydin@turkdeveloper.com>
2017-12-22 17:13:22 +0300

    front-end design

---
+++ client/js/coming-soon.js
@@ -0,0 +1,4 @@
+// Service Worker installing...
+require('offline-plugin/runtime').install();
+
+import "../less/coming-soon/main.less";
---
+++ client/js/maintenance.js
@@ -0,0 +1,4 @@
+// Service Worker installing...
+require('offline-plugin/runtime').install();
+
+import "../less/maintenance/main.less";
