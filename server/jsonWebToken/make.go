3cd43eda4f72882db28c538ca8faab2cc6aeabd9
Muhammed Furkan Aydın <furkanaydin@turkdeveloper.com>
2018-01-12 15:20:01 +0300

    created make function for jwt

--- server/jsonWebToken/make.go
+++ server/jsonWebToken/make.go
@@ -4,16 +4,9 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-func Make() (string, error) {
-	type User struct {
-		jwt.StandardClaims
-		Name string `json:"name"`
-		Age  int    `json:"age"`
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &User{
-		Name: "otiai10",
-		Age:  30,
-	})
+func Make(tokenSource jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, tokenSource)
+
 	hmacSampleSecret := []byte("t1LrbFpKG5v4ENntnFeUWHOAY7cYHLUS")
 	// Sign and get the complete encoded token as a string using the secret
 	tokenString, err := token.SignedString(hmacSampleSecret)
