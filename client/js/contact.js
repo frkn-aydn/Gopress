ccfec9d42b2da3565ae3d7f9037940d5d1d55a0b
Muhammed Furkan Aydın <furkanaydin@turkdeveloper.com>
2018-01-23 17:49:23 +0300

    making ajax call when form submited

--- client/js/contact.js
+++ client/js/contact.js
@@ -4,32 +4,30 @@ import "../less/landing/main.less";
 
 import "./template-utils/menu.js";
 
-document.addEventListener("DOMContentLoaded", _ => {
-    document.getElementById("contact-form").addEventListener("submit", e => {
-        e.preventDefault();
+document.getElementById("contact-form").addEventListener("submit", e => {
+    e.preventDefault();
 
-        const name = document.getElementById("name");
-        const email = document.getElementById("email");
-        const message = document.getElementById("message");
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
 
-        const captcha = grecaptcha.getResponse();
+    const captcha = grecaptcha.getResponse();
 
-        console.log(captcha)
+    console.log(captcha)
 
-        if (!captcha) return alert("I'm just wondering? Are you Robot ?")
-        if (!name || !email || !message) return alert("Missing fields.");
+    if (!captcha) return alert("I'm just wondering? Are you Robot ?")
+    if (!name || !email || !message) return alert("Missing fields.");
 
-        const ajax = new XMLHttpRequest();
-        ajax.open("POST", "/api/contact", true);
-        ajax.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        ajax.onload = function () {
-            console.log(ajax.response)
-        }
-        ajax.send(JSON.stringify({
-            Email: email.value,
-            Name: name.value,
-            Message: message.value,
-            Captcha: captcha
-        }))
-    })
-})
+    const ajax = new XMLHttpRequest();
+    ajax.open("POST", "/api/contact", true);
+    ajax.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    ajax.onload = function () {
+        console.log(ajax.response)
+    }
+    ajax.send(JSON.stringify({
+        Email: email.value,
+        Fullname: name.value,
+        Message: message.value,
+        Captcha: captcha
+    }))
+}, false)
